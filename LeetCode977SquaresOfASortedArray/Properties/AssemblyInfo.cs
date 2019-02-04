343297bbefeb4a71e5d5448d65f613cde97a5bc1
givemin5 <givemin5@gmail.com>
2019-02-04 23:31:17 +0800

    Add project files.

---
+++ LeetCode977SquaresOfASortedArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCode977SquaresOfASortedArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode977SquaresOfASortedArray")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42eee777-7da0-48bf-8214-2c9c5ffd68ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCode977SquaresOfASortedArray/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode977SquaresOfASortedArray
+{
+    public class Solution
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
