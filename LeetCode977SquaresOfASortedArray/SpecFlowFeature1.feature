8040be06c115d9cf375b40bebc95072d937360b7
givemin5 <givemin5@gmail.com>
2019-02-05 13:48:08 +0800

    Add Specflow Test

---
+++ LeetCode977SquaresOfASortedArray/SpecFlowFeature1.feature
@@ -0,0 +1,38 @@
+﻿Feature:  Squares Of a Sorted Array
+	In order to 正確計算平方值並排序
+	As 身為一個使用者
+	I want to 我希望能輸入陣列資料後輸出平方值並排序
+@mytag
+Scenario: Squares Of a Sorted Array Example 1
+	Given I Enter Array into the SortedSquares
+	| A  |
+	| -4 |
+	| -1 |
+	| 0  |
+	| 3  |
+	| 10 |
+	When I exec SortedSquares
+	Then the result should be 
+	| Result |
+	| 0      |
+	| 1      |
+	| 9      |
+	| 16     |
+	| 100    |
+
+Scenario: Squares Of a Sorted Array Example 2
+	Given I Enter Array into the SortedSquares
+	| A  |
+	| -7 |
+	| -3 |
+	| 2  |
+	| 3  |
+	| 11 |
+	When I exec SortedSquares
+	Then the result should be 
+	| Result |
+	| 4      |
+	| 9      |
+	| 9      |
+	| 49     |
+	| 121    |
---
+++ LeetCode977SquaresOfASortedArray/SpecFlowFeature1Steps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using ExpectedObjects;
+using TechTalk.SpecFlow;
+
+namespace LeetCode977SquaresOfASortedArray
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I Enter Array into the SortedSquares")]
+        public void GivenIEnterArrayIntoTheSortedSquares(Table table)
+        {
+            var A = table.Rows.Select(r => Convert.ToInt32(r["A"])).ToArray();
+            ScenarioContext.Current.Add("A",A);
+        }
+        
+        [When(@"I exec SortedSquares")]
+        public void WhenIExecSortedSquares()
+        {
+            var A = ScenarioContext.Current.Get<int[]>("A");
+
+            Solution solution = new Solution();
+            var actual = solution.SortedSquares(A);
+
+            ScenarioContext.Current.Add("actual", actual);
+
+        }
+        
+        [Then(@"the result should be")]
+        public void ThenTheResultShouldBe(Table table)
+        {
+            var expected = table.Rows.Select(r => Convert.ToInt32(r["Result"])).ToArray();
+
+            var actual = ScenarioContext.Current.Get<int[]>("actual");
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
